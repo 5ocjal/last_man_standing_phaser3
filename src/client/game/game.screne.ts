090dacce2ae29f022f6e4f3e5c690c914ea7a99b
Rafał Tupacz <r.tupacz@gmail.com>
2020-06-08 10:30:08 +0200

    player move

--- src/client/game/game.screne.ts
+++ src/client/game/game.screne.ts
@@ -4,7 +4,14 @@ export class GameScene extends Phaser.Scene {
   public actors = [];
   public actor: any;
   public game: Phaser.Game;
-  public cursors;
+  public control;
+  public pointer;
+
+  public rotationSpeed = 1 * Math.PI;
+  public rotationDegrees = Phaser.Math.RadToDeg(this.rotationSpeed);
+  public rotationTolerance = 0.02 * this.rotationSpeed;
+  public velocityFromRotation = Phaser.Physics.Arcade.ArcadePhysics.prototype.velocityFromRotation;
+
   protected manageAssets(): void {}
   protected gameUpdate(): void {
     if (this.actor && this.actor.controls) {
@@ -16,18 +23,19 @@ export class GameScene extends Phaser.Scene {
     console.log('preload');
     this.load.image('ground', '../public/assets/map/ground.png');
     this.load.spritesheet('idle', '../public/assets/sprites/idle.png', {
-      frameWidth: 363,
-      frameHeight: 597,
+      frameWidth: 597,
+      frameHeight: 363,
     });
     this.load.spritesheet('walk', '../public/assets/sprites/walk.png', {
-      frameWidth: 354,
-      frameHeight: 586,
+      frameWidth: 586,
+      frameHeight: 354,
     });
   }
 
   public create() {
     console.log('create');
     this.physics.world.setBounds(0, 0, 800, 600);
+
     this.add.image(400, 300, 'ground');
 
     this.anims.create({
@@ -54,22 +62,26 @@ export class GameScene extends Phaser.Scene {
     this.actors.push[this.actor];
     this.cameras.main.fadeIn(4000, 247, 208, 36);
     this.cameras.main.startFollow(this.actor.player);
-    this.cursors = this.input.keyboard.createCursorKeys();
+    this.pointer = this.input.activePointer;
   }
 
   public update() {
-    if (this.cursors.down.isDown) {
-      this.actor.player.setVelocityY(100);
-    } else if (this.cursors.up.isDown) {
-      this.actor.player.setVelocityY(-100);
-    } else if (this.cursors.left.isDown) {
-      this.actor.player.setVelocityX(-100);
-    } else if (this.cursors.right.isDown) {
-      this.actor.player.setVelocityX(100);
-    } else {
-      this.actor.player.setVelocity(0);
-    }
+    //  if (this.cursors.down.isDown) {
+    //    this.actor.player.setVelocityY(100);
+    //  } else if (this.cursors.up.isDown) {
+    //    this.actor.player.setVelocityY(-100);
+    //  } else if (this.cursors.left.isDown) {
+    //    this.actor.player.setVelocityX(-100);
+    //  } else if (this.cursors.right.isDown) {
+    //    this.actor.player.setVelocityX(100);
+    //  } else {
+    //    this.actor.player.setVelocity(0);
+    //  }
 
+    this.pointerMove();
+    this.pointer.isDown
+      ? this.physics.moveTo(this.actor.player, this.pointer.worldX, this.pointer.worldY, 100)
+      : this.physics.moveTo(this.actor.player, this.pointer.worldX, this.pointer.worldY, 0);
   }
 
   protected properties(): void {
@@ -88,5 +100,22 @@ export class GameScene extends Phaser.Scene {
       },
       this
     );
+  }
+
+  protected pointerMove() {
+    let angleToPointer = Phaser.Math.Angle.Between(
+      this.actor.player.x,
+      this.actor.player.y,
+      this.pointer.worldX,
+      this.pointer.worldY
+    );
+    let angleDelta = Phaser.Math.Angle.Wrap(angleToPointer - this.actor.player.rotation);
+
+    if (Phaser.Math.Within(angleDelta, 0, this.rotationTolerance)) {
+      this.actor.player.rotation = angleToPointer;
+      this.actor.player.setAngularVelocity(0);
+    } else {
+      this.actor.player.setAngularVelocity(Math.sign(angleDelta) * this.rotationDegrees);
+    }
   }
 }
